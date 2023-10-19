f4632ab41bf6a2ee51068f8434dd09ed630fa428
alexand-1 <andrey.dimidov98@mail.ru>
2023-10-19 18:48:25 +0300

    new load

--- WebServiseFiles/main/urls.py
+++ WebServiseFiles/main/urls.py
@@ -4,6 +4,7 @@ from allauth.account.views import LoginView, SignupView, LogoutView
 
 urlpatterns = [
     path('',views.index),
+    path('home/', views.home, name= 'home'),
     path('accounts/login/', LoginView.as_view(), name='account_login'),
     path('accounts/signup/', SignupView.as_view(), name='account_signup'),
     path('logout/', LogoutView.as_view(), name='account_logout'),
